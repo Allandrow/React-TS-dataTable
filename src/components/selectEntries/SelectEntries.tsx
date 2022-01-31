6b17596432546902cf04ea789d7a0c53ea208ee7
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-01-31 16:27:33 +0100

    refaco SelectEntries with options as optional props

--- src/components/selectEntries/SelectEntries.tsx
+++ src/components/selectEntries/SelectEntries.tsx
@@ -1,18 +1,23 @@
 import { FormEvent } from 'react'
 
 interface SelectEntriesProps {
+  options?: number[]
   callback: (e: FormEvent<HTMLSelectElement>) => void
 }
 
-export const SelectEntries = ({ callback }: SelectEntriesProps) => {
+export const SelectEntries = ({
+  options = [10, 20, 50, 100],
+  callback,
+}: SelectEntriesProps) => {
   return (
     <label>
       <span>Show</span>
       <select name="entries" onChange={callback}>
-        <option value="10">10</option>
-        <option value="20">20</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
+        {options.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
       </select>
       <span>entries</span>
     </label>
