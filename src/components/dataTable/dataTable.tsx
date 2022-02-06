3d3c4cdc052954b8dfcbf62ea7600a23bf732332
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-02-06 18:55:09 +0100

    comments setting up components

---
+++ src/components/dataTable/dataTable.tsx
@@ -0,0 +1,35 @@
+/*
+  Props :
+    data
+    headings
+*/
+
+export const DataTable = () => {
+  /*
+    state :
+      - searchValue
+      - pageSize
+      - currentPage
+      - ordering
+      - displayedData
+  */
+
+  // select
+  // search
+  /*
+    Table :
+      - headings with ordering
+      - displayedData
+  */
+  // recap
+  // pagination
+
+  return <h1>WIP</h1>
+}
+
+/*
+  rerenders :
+    - displayedData when pageSize / searchValue / ordering / currentPage changes
+    - pagination when pageSize / displayedData / currentPage changes
+    - recap when currentPage / pageSize / searchValue changes
+*/
