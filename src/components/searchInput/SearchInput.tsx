f5f4ff29c881ae814d21acbb9b063e0e6980cd82
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-02-09 15:21:49 +0100

    revert to uncontrolled component

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -1,23 +1,14 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent } from 'react'
 
 type SearchInputProps = {
-  callback: (value: string) => void
+  callback: (e: FormEvent<HTMLInputElement>) => void
 }
 
 export const SearchInput = ({ callback }: SearchInputProps) => {
-  const [searchValue, setSearchValue] = useState('')
-
-  // TODO : is it really needed to handle local state ?
-  const handleChange = (e: FormEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value
-    setSearchValue(value)
-    callback(value)
-  }
-
   return (
     <label>
       <span>Search: </span>
-      <input type="search" value={searchValue} onInput={handleChange} />
+      <input type="search" onChange={callback} />
     </label>
   )
 }
