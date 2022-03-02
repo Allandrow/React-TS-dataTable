a3497bd06210d135d93462c8de28803fa90372c4
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-03-02 13:05:10 +0100

    added map with sort methods

--- src/hooks/useSorting.tsx
+++ src/hooks/useSorting.tsx
@@ -1,4 +1,5 @@
 import { Column, Data, Sorting } from '../types'
+import { sortingFunctions } from '../utils/sortFunctions'
 
 interface SortingProps {
   data: Data[]
@@ -6,73 +7,12 @@ interface SortingProps {
   columns: Column[]
 }
 
-const isDate = (value: unknown) => {
-  if (typeof value === 'string') {
-    return isFinite(Date.parse(value))
-  }
-  if (value instanceof Date) {
-    return isFinite(value.getTime())
-  }
-
-  return false
-}
-
-const sortNumber = (a: Data, b: Data, { key, direction }: Sorting) => {
-  const aValue = a[key]
-  const bValue = b[key]
-
-  if (typeof aValue === 'number' && typeof bValue === 'number') {
-    if (aValue < bValue) return direction === 'ascending' ? 1 : -1
-    if (aValue > bValue) return direction === 'ascending' ? -1 : 1
-    return 0
-  }
-
-  throw new Error(
-    `At least one of the two compared values from key : ${key} are not of type number for this sort.`
-  )
-}
-
-const sortDate = (a: Data, b: Data, { key, direction }: Sorting) => {
-  const aValue = a[key]
-  const bValue = b[key]
-
-  if (isDate(aValue) && isDate(bValue)) {
-    const aDate = new Date(aValue as string | Date)
-    const bDate = new Date(bValue as string | Date)
-
-    if (aDate < bDate) return direction === 'ascending' ? 1 : -1
-    if (aDate > bDate) return direction === 'ascending' ? -1 : 1
-    return 0
-  }
-
-  throw new Error(
-    `At least one of the two compared values from key : ${key} are not a valid string date or date object for this sort.`
-  )
-}
-
-const sortString = (a: Data, b: Data, { key, direction }: Sorting) => {
-  const aValue = a[key]
-  const bValue = b[key]
-
-  if (typeof aValue === 'string' && typeof bValue === 'string') {
-    const aString = aValue.toLowerCase()
-    const bString = bValue.toLowerCase()
-
-    if (aString < bString) return direction === 'ascending' ? 1 : -1
-    if (aString > bString) return direction === 'ascending' ? -1 : 1
-    return 0
-  }
-
-  throw new Error(
-    `At least one of the two compared values from key : ${key} are not of type string for this sort.`
-  )
-}
-
 export const useSorting = ({ data, sorting, columns }: SortingProps) => {
   const { sortMethod } = columns.find((column) => column.key === sorting.key)!
 
-  if (sortMethod === 'sortNumber') return data.sort((a, b) => sortNumber(a, b, sorting))
-  if (sortMethod === 'sortDate') return data.sort((a, b) => sortDate(a, b, sorting))
+  if (sortMethod === undefined) {
+    return data.sort((a, b) => sortingFunctions.get('sortString')(a, b, sorting))
+  }
 
-  return data.sort((a, b) => sortString(a, b, sorting))
+  return data.sort((a, b) => sortingFunctions.get(sortMethod)(a, b, sorting))
 }
