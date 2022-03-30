153debd12e7e74aeda23318bcea822dcac244755
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-03-30 11:31:20 +0200

    added function typeguard + refacto conditionals in useSorting for sortMethod value/type

--- src/hooks/useSorting/useSorting.tsx
+++ src/hooks/useSorting/useSorting.tsx
@@ -1,5 +1,5 @@
 import { sortFunctions } from '../../utils'
-import { Data, Column, SortBy } from '../../types'
+import { Data, Column, SortBy, UserSortMethod } from '../../types'
 
 interface SortingProps {
   data: Data[]
@@ -7,26 +7,15 @@ interface SortingProps {
   columns: Column[]
 }
 
+const isSortingFunction = (fnc: unknown): fnc is UserSortMethod => {
+  return fnc instanceof Function
+}
+
 export const useSorting = ({ data, sorting, columns }: SortingProps) => {
   const { sortMethod } = columns.find((column) => column.id === sorting.id)!
 
   if (sortMethod) {
-    if (typeof sortMethod === 'string') {
-      if (sortFunctions.has(sortMethod)) {
-        return [...data].sort((a, b) => sortFunctions.get(sortMethod)(a, b, sorting))
-      } else {
-        console.error(
-          `${sortMethod} is not a known helper sorting method included in this library. \n
-          Known methods are : \n
-          - sortString
-          - sortNumber
-          - sortDateISO`
-        )
-        throw new Error('Invalid sortMethod name')
-      }
-    }
-
-    if (sortMethod instanceof Function) {
+    if (isSortingFunction(sortMethod)) {
       try {
         return [...data].sort((a, b) => sortMethod(a, b, sorting))
       } catch (err) {
@@ -34,6 +23,19 @@ export const useSorting = ({ data, sorting, columns }: SortingProps) => {
         throw new Error('Something went wrong with the function used to sort data')
       }
     }
+
+    if (typeof sortMethod === 'string' && sortFunctions.has(sortMethod)) {
+      return [...data].sort((a, b) => sortFunctions.get(sortMethod)(a, b, sorting))
+    }
+
+    console.error(
+      `${sortMethod} is not a known helper sorting method included in this library.
+        Known methods are :
+        - sortString
+        - sortNumber
+        - sortDateISO`
+    )
+    throw new Error('Invalid sortMethod name')
   }
 
   return [...data].sort((a, b) => sortFunctions.get('sortString')(a, b, sorting))
